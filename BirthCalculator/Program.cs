b9b703e464c4c679d7e86a469011498b79b87001
Muslim O.Ahmad <muslem84omer@gmail.com>
2021-01-13 14:11:29 +0300

    Project imported

---
+++ BirthCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateOfBirthCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("                            Year Of Birth Calculator                       ");
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            Console.Write("-> Enter Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int currentYear = DateTime.Now.Year;
+
+            int yearOfBirth = currentYear - age;
+
+            Console.WriteLine("-> Your Year of Birth is: " + yearOfBirth);
+
+            Console.WriteLine();
+        }
+    }
+}
